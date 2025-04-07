9c23c6b3d51b640a327423380719c2b0f584e4ea
Pranav Kumar <pranavkumar2025.pmd@gmail.com>
2025-04-07 15:16:40 +0530

    New Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Header1 from "./components/Header/Header1";
 import Header2 from "./components/Header/Header2";
+import Publication from "./components/Publication";
 import Footer1 from "./components/Footer/Footer1";
 import Footer2 from "./components/Footer/Footer2";
+
 const App = () => {
   return (
-			<Router>
-				{/* <Header /> */}
-				<Header1 />
-				<Header2 />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/publication" element={<Publication />} />
-					{/* Add other routes */}
-				</Routes>
-        <Footer1/>
-        <Footer2/>
-			</Router>
-		);
-
+    <Router>
+      {/* <Header /> */}
+      <Header1 />
+      <Header2 />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/publication" element={<Publication />} />
+        {/* Add other routes */}
+      </Routes>
+      <Footer1 />
+      <Footer2 />
+    </Router>
+  );
 };
 
 export default App;
